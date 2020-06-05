c1c7892a218b434d254e8addd9e41dad99d1349b
sandip chaudhary <sndp.c114@gmail.com>
2020-06-05 14:00:42 +0545

    Updates regarding Create, Update and Delete operations for the post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 #from django.http import HttpResponse
 
@@ -25,11 +33,58 @@ from .models import Post
 #     }
 # ]
 
+#home function
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+#home ListView
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-posted_date']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostDetailView(DetailView):
+    model = Post
+
+#UpdateView is similar to CreateView
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:   #self.request.user this gets the currently login user
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:   #self.request.user this gets the currently login user
+            return True
+        return False
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About' })
